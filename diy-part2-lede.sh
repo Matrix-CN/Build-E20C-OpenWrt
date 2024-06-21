214dc0aa0c7cc4fe6de600176906701fe7161f86
Ming <57690855+ZZM365-CN@users.noreply.github.com>
2024-06-21 22:26:24 +0800

    Update and rename diy-part2.sh to diy-part2-lede.sh

---
+++ diy-part2-lede.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1g' package/base-files/files/bin/config_generate
+
+# Add build date to index page
+export orig_version="$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
+sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" package/lean/default-settings/files/zzz-default-settings
+
+# Add luci-theme-argon
+rm -rf ./feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+
+# Add luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+
+./scripts/feeds update -a
+./scripts/feeds install -a
