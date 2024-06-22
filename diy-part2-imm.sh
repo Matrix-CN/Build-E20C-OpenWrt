652fdd920ddd09c4dad49b039f36103e47172d84
Ming <57690855+ZZM365-CN@users.noreply.github.com>
2024-06-22 17:10:46 +0800

    Create diy-part2-imm.sh

---
+++ diy-part2-imm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1g' package/base-files/files/bin/config_generate
+
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果)
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Add luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+
+./scripts/feeds update -a
+./scripts/feeds install -a
